2984b6e0c0c778ccd5089cfe3f75d3bd11b6b1e5
Cristian Milani <cristian.milani@alten.it>
2024-11-28 12:00:04 +0100

    First commit

---
+++ lua/options.lua
@@ -0,0 +1,33 @@
+require "nvchad.options"
+
+-- Show line numbers
+vim.o.number = true
+
+-- Show relative line numbers
+vim.o.relativenumber = true
+
+-- Show a vertical line at column 80
+vim.o.colorcolumn = "80"
+
+-- Highlight line under the cursor
+vim.opt.cursorlineopt = "both"
+
+-- Show existing tabs with 4 spaces
+vim.o.tabstop = 4
+
+-- Use 4 spaces when indenting with << or >>
+vim.o.shiftwidth = 4
+
+-- When pressing tab, insert "shiftwidth" spaces instead
+vim.o.expandtab = true
+
+-- Keep indentation when line breaks
+vim.o.breakindent = true
+
+-- Minimum number of lines to keep above and below the cursor
+vim.o.scrolloff = 15
+
+-- 
+vim.opt.clipboard = "unnamed"
+
+vim.o.termguicolors = true
