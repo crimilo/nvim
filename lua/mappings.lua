982b6571c69652dd790da3dbf7196caf7167f5c2
Cristian Milani <111324740+crimilo@users.noreply.github.com>
2024-12-19 19:20:04 +0100

    Add lsp format keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,11 +2,13 @@ require "nvchad.mappings"
 
 -- add yours here
 
--- local map = vim.keymap.set
+local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-vim.api.nvim_set_keymap('i', '<Tab>', '<Tab>', { noremap = true, silent = true })
+map('i', '<Tab>', '<Tab>', { noremap = true, silent = true })
+
+map('n', '<Space>k', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
